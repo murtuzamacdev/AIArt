43dd3a3464097f01d409faf401c13b7cd349e83c
Murtuza Chakkiwala <murtuza.mac@gmail.com>
2023-03-08 12:44:34 +0530

    Initial Commit

---
+++ AI Art Image Generator/AI_Art_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Art_Image_GeneratorApp.swift
+//  AI Art Image Generator
+//
+//  Created by Murtuza Chakkiwala on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_Art_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
